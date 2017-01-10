1f32fc60be3bb0f55e6088d3d0417c910e50b73c
Eric Holscher <eric@ericholscher.com>
2017-01-10 13:57:29 -0800

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.findanything']
+extensions = []]
 
 
 # Add any paths that contain templates here, relative to this directory.

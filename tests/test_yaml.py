821dc25ea0a84f0d918434acf5aedf98fb4a0552
Jinheng Wang <jinhwa@microsoft.com>
2017-05-05 11:25:43 +0800

    fix CI test case

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -172,7 +172,7 @@ class PythonTests(unittest.TestCase):
                             'The Foo instance is destructed'
                         )
                         self.assertEqual(
-                            item['seealso'],
+                            item['seealsoContent'],
                             'See also: Some cool stuff online.'
                         )
 
@@ -186,7 +186,7 @@ class PythonTests(unittest.TestCase):
                 for item in data['items']:
                     if item['uid'] == 'example.nap.Base.ref':
                         self.assertEqual(
-                            item['seealso'],
+                            item['seealsoContent'],
                             'See also: Depends on @example.example.Foo'
                         )
 

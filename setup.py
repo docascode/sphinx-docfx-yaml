b495e423bb215a802f1721626a5d6101697a132e
Yiding Tian <jaytin1218@hotmail.com>
2018-09-06 16:29:02 +0800

    [mod] Update version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require=['pytest', 'mock'],
 
 setup(
     name='sphinx-docfx-yaml',
-    version='1.2.47',
+    version='1.2.48',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://github.com/ericholscher/sphinx-docfx-yaml',

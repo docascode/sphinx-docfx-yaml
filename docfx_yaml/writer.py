a527aad98d60779e6ee256b8b6ed041c8c127335
Yiding Tian <jaytin1218@hotmail.com>
2018-09-17 12:09:00 +0800

    [fix] Fix URL link not correctly rendered.

--- docfx_yaml/writer.py
+++ docfx_yaml/writer.py
@@ -891,7 +891,7 @@ class MarkdownTranslator(nodes.NodeVisitor):
         if 'refid' in node.attributes:
             self.add_text('<xref:{}>'.format(node.attributes['refid']))
         elif 'refuri' in node.attributes:
-            if 'http' in node.attributes['refuri']:
+            if 'http' in node.attributes['refuri'] or node.attributes['refuri'][0] == '/':
                 self.add_text('[{}]({})'.format(node.astext(), node.attributes['refuri']))
             else:
                 # only use id in class and func refuri if its id exists

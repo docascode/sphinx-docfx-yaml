841c43f85213c2127e23189c37c6c3de38710a92
Yiding Tian <jaytin1218@hotmail.com>
2018-09-17 14:27:27 +0800

    [fix] Fix line wrapping in remarks

--- docfx_yaml/nodes.py
+++ docfx_yaml/nodes.py
@@ -6,7 +6,7 @@ This module is used to add extra supported nodes to sphinx.
 
 from docutils import nodes
 
-class remarks(nodes.Admonition, nodes.Element):
+class remarks(nodes.paragraph, nodes.Element):
     """
     New node for remarks messages.
     """

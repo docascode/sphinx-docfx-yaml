19c6a16c1b919bac33c8ec11c5ecf5bf860dc89e
Terry Jin <terryjin@microsoft.com>
2019-10-23 09:55:18 +0800

    update package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require=['pytest', 'mock'],
 
 setup(
     name='sphinx-docfx-yaml',
-    version='1.2.70',
+    version='1.2.71',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://github.com/ericholscher/sphinx-docfx-yaml',
